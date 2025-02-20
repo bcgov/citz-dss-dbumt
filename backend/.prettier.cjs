646c46476dab8949df8333f8a72ca0a25eff7dbb
taylorfries <taylor.friesen@gov.bc.ca>
2025-02-20 07:30:09 -0800

    add prettier configs to backend

---
+++ backend/.prettier.cjs
@@ -0,0 +1,11 @@
+/* eslint-disable no-undef */
+module.exports = {
+  semi: true,
+  trailingComma: 'all',
+  singleQuote: true,
+  printWidth: 100,
+  tabWidth: 2,
+  endOfLine: 'lf',
+  arrowParens: 'always',
+  bracketSpacing: true,
+};
