e091c6ea7a9ce3db6e623675f17ad60b2f5c6843
taylorfries <taylor.friesen@gov.bc.ca>
2025-02-27 14:10:21 -0800

    add connection function

--- backend/src/config/env.ts
+++ backend/src/config/env.ts
@@ -1,19 +1,19 @@
 // Environment variables set in the .env file.
 const {
+  APPLICATION_NAME,
   BACKEND_PORT,
   MONGO_EXTERNAL_PORT,
   MONGO_USER,
   MONGO_PASSWORD,
-  MONGO_DATABASE_NAME,
-  MONGO_HOST,
+  MONGO_SERVICE_NAME,
 } = process.env;
 
 // Exported configuration values.
 export default {
+  APPLICATION_NAME,
   PORT: BACKEND_PORT ? Number(BACKEND_PORT) : 3200,
   MONGO_EXTERNAL_PORT,
   MONGO_USER,
   MONGO_PASSWORD,
-  MONGO_DATABASE_NAME,
-  MONGO_HOST,
+  MONGO_SERVICE_NAME,
 };
--- backend/src/middleware/database.ts
+++ backend/src/middleware/database.ts
@@ -2,21 +2,24 @@ import mongoose from "mongoose";
 import { ENV } from "../config"
 
 // set env variables from .env to process.env
-const { MONGO_USER, MONGO_PASSWORD, MONGO_DATABASE_NAME, MONGO_EXTERNAL_PORT, MONGO_HOST } =
+const { MONGO_USER, MONGO_PASSWORD, APPLICATION_NAME, MONGO_EXTERNAL_PORT, MONGO_SERVICE_NAME } =
   ENV;
 
 export const connectDatabase = async () => {
-  if (!(MONGO_HOST && MONGO_DATABASE_NAME && MONGO_USER && MONGO_PASSWORD))
+  if (!(MONGO_SERVICE_NAME && APPLICATION_NAME && MONGO_USER && MONGO_PASSWORD))
     throw new Error(
       "One or more of [MONGO_USER, MONGO_PASSWORD, MONGO_DATABASE_NAME, MONGO_HOST] env vars is undefined."
     );
 
   try {
-    // if we are running in docker set the uri to use 'mongod' otherwise use localhost
-    const hostEnv = process.env.NODE_ENV === 'docker' ? 'mongodb' : 'localhost'
-    const databaseUri = `mongodb://${hostEnv}:${MONGO_EXTERNAL_PORT}/${MONGO_HOST}`
+    const localDBUri = `localhost:${MONGO_EXTERNAL_PORT}/${MONGO_SERVICE_NAME}`
+    const containerDBUri = `${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_SERVICE_NAME}:${MONGO_EXTERNAL_PORT}/${APPLICATION_NAME}?authSource=admin`
 
-    const connection = await mongoose.connect(databaseUri)
+    // if we are running in docker set the uri to use container URI otherwise use local URI
+    const mongoURI = process.env.NODE_ENV === 'development' ? containerDBUri : localDBUri
+    console.log("attempting to connect to MongoDB with: ", `mongodb://${mongoURI}`)
+
+    const connection = await mongoose.connect(`mongodb://${mongoURI}`)
     if (connection)
       console.log("Connected to MongoDB")
   } catch (err) {
