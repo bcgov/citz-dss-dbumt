4302c0f6d1845e8d304d8ce18ba2d9491acf7e9a
taylorfries <taylor.friesen@gov.bc.ca>
2025-02-28 14:52:26 -0800

    add logger

---
+++ backend/src/utilities/logs.ts
@@ -0,0 +1,12 @@
+import { ANSI_CODES } from "@/constants";
+
+// unpack constants from @/constants/ansiCodes.ts
+// Note: FG.<colour> will update console letter colours
+//       BG.<colour> will update the background colour
+//       FM.<option> will apply to the terminal
+const { FOREGROUND: FG, BACKGROUND: BG, FORMATTING: FM } = ANSI_CODES;
+
+const expressJS = ` ${BG.CYAN}${FG.GREY}| ExpressJS |${FM.RESET} `;
+export const API = {
+  SERVER_STARTED: `${expressJS}${FG.LIME}DBUMT Server Started Successfully. Listening on port: ${FM.RESET}`,
+};
