7c2f7e87bd2099f1233d57656d7bc52b10f9231d
taylorfries <taylor.friesen@gov.bc.ca>
2025-03-05 15:18:55 -0800

    formatting fix

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -3,12 +3,12 @@ import { Heading, Text } from "@bcgov/design-system-react-components";
 export const Home = () => {
   return (
     <>
-      <Heading level={1}>
-        Database User Management Tool
-      </Heading>
+      <Heading level={1}>Database User Management Tool</Heading>
       <Text elementType="span">
-        This page is the landing page. Currently it isnt very exciting. But we can use this to test building out components and will build onto it as we go.
+        This page is the landing page. Currently it isnt very exciting. But we
+        can use this to test building out components and will build onto it as
+        we go.
       </Text>
     </>
-  )
-}
+  );
+};
