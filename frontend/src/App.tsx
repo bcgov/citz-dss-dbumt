964264294a8f74df4c253072a8bc7b33e0ccc2fb
Molnar <Stuart.Molnar@gov.bc.ca>
2025-03-18 11:29:34 -0700

    Create page header

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,14 @@
-import { useState } from "react";
+import { Home } from "./pages/Home";
+import { Header } from "@bcgov/design-system-react-components";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <h1>DBUMT</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-      </div>
-      <p> More information to come...</p>
+      <Header
+        title="Database User Management Tool"
+        titleElement="h1"
+      />
+      <Home />
     </>
   );
 }
---
+++ frontend/src/pages/Home.tsx
@@ -0,0 +1,15 @@
+import { Text } from "@bcgov/design-system-react-components";
+import AppHeader from "../components/AppHeader";
+
+
+export const Home = () => {
+  return (
+    <>
+      <Text elementType="span">
+        This page is the landing page. Currently it isnt very exciting. But we
+        can use this to test building out components and will build onto it as
+        we go.
+      </Text>
+    </>
+  );
+};
