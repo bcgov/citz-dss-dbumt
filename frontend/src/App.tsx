af7250e74577ad7b451d9f0c650790becf7f0ba1
Molnar <Stuart.Molnar@gov.bc.ca>
2025-03-18 11:50:07 -0700

    Create page footer

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,24 @@
+import React from "react";
 import { Home } from "./pages/Home";
+import { Footer, FooterLinks } from "@bcgov/design-system-react-components";
 
 function App() {
   return (
     <>
       <Home />
+      <Footer>
+        <React.Fragment key=".0">
+          <FooterLinks
+            links={[
+              <a href="https://www2.gov.bc.ca/gov/content/home/disclaimer">Disclaimer</a>,
+              <a href="https://www2.gov.bc.ca/gov/content/home/privacy">Privacy</a>,
+              <a href="https://www2.gov.bc.ca/gov/content/home/copyright">Copyright</a>,
+              <a href="https://www2.gov.bc.ca/gov/content/home/get-help-with-government-services">Contact Us</a>
+            ]}
+            title=""
+          />
+        </React.Fragment>
+      </Footer>
     </>
   );
 }
