e5901d727ce9ef7d21f9d46607bffae014ec47b0
Molnar <Stuart.Molnar@gov.bc.ca>
2025-03-18 12:54:05 -0700

    Remove unused "Heading" component

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,10 +1,8 @@
 import { Text } from "@bcgov/design-system-react-components";
-import { Heading, Text } from "@bcgov/design-system-react-components";
 
 export const Home = () => {
   return (
     <>
-      <Heading level={1}>Database User Management Tool</Heading>
       <Text elementType="span">
         This page is the landing page. Currently it isnt very exciting. But we
         can use this to test building out components and will build onto it as
