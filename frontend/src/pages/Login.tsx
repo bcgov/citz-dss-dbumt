3acd2f675c38dcdafd47f286103d01933bb5a539
Molnar <Stuart.Molnar@gov.bc.ca>
2025-04-09 13:28:15 -0700

    Create Login.tsx

---
+++ frontend/src/pages/Login.tsx
@@ -0,0 +1,23 @@
+import { Heading, Text } from '@bcgov/design-system-react-components';
+import { BaseLayout } from '../components/layout/BaseLayout';
+
+export const Login = () => {
+  return (
+    <BaseLayout>
+      <Heading level={1}>Login Page</Heading>
+      <Text>
+        <p>
+          This page uses the `Base Layout` wrapper allowing content to appear
+          between the header and footer.
+        </p>
+        <br />
+        <Heading level={3}>More Content</Heading>
+        <p>
+          This page is the landing page. Currently it isnt very exciting. But we
+          can use this to test building out components and will build onto it as
+          we go.
+        </p>
+      </Text>
+    </BaseLayout>
+  );
+};
