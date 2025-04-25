16caaaf173db6b405cb09c3f586df54b06384508
Taylor Friesen <taylor.friesen@gov.bc.ca>
2025-04-25 13:30:35 -0700

    update home and login page to use new infobox

---
+++ frontend/src/components/element/InfoBox.tsx
@@ -0,0 +1,84 @@
+import { ReactNode } from 'react';
+
+/**
+ * InfoBoxProps interface defines the properties for the InfoBox component.
+ *  header: string - The header text to be displayed in the InfoBox
+ *  children: ReactNode - Optional child elements to be displayed inside the InfoBox
+ */
+interface InfoBoxProps {
+  header: string;
+  children?: ReactNode;
+}
+
+/**
+ * InfoBoxFieldProps interface defines the properties for the InfoBoxField component.
+ *  titleText: string - The title text to be displayed
+ *  contentText?: string - Optional content text to be displayed to the right of the title
+ */
+interface InfoBoxFieldProps {
+  titleText: string;
+  contentText?: string;
+}
+
+/**
+ * InfoBox component that serves as a container for sub-components. Displays a box following
+ *  BC Gov design system guidelines with a header and optional child elements inside.
+ * When possilbe use the BC Gov design system components instead of creating
+ *  new custom sum-components. Ex. Button, TextField, etc.
+ *
+ * @param props - InfoBoxProps object containing header and optional children
+ * @returns JSX.Element - The rendered InfoBox component
+ */
+export const InfoBox = (props: InfoBoxProps) => {
+  return (
+    <div className="bg-grey shadow-sm">
+      <InfoBoxHeader headerText={props.header} />
+      <div className="mx-[27px] mt-[27px] pb-[50px] font-[BC_Sans]">
+        {props.children}
+      </div>
+    </div>
+  );
+};
+
+/**
+ * InfoBoxHeader component that displays the header of the InfoBox
+ * The header is styled with a blue background and white text
+ *
+ * @param headerText: string - The text to be displayed in the header
+ * @returns JSX.Element - The rendered InfoBoxHeader component
+ */
+export const InfoBoxHeader = ({ headerText }: { headerText: string }) => {
+  return (
+    <div className="bg-blue flex h-[65px] items-center justify-start">
+      <p className="mx-[17px] text-[20px] font-[BC_Sans] font-bold text-white">
+        {headerText}
+      </p>
+    </div>
+  );
+};
+
+/**
+ * InfoBoxFieldTitle component that displays the title of a field
+ *
+ * @param titleText: string - The text to be displayed as the title of the field
+ * @returns JSX.Element - The rendered InfoBoxFieldTitle component
+ */
+export const InfoBoxFieldTitle = ({ titleText }: { titleText: string }) => {
+  return <p className="text-[16px] font-[BC_Sans] font-bold">{titleText}</p>;
+};
+
+/**
+ * InfoBoxField component that displays a field with a title and content
+ * It uses the InfoBoxFieldTitle component to display the title
+ *
+ * @param props - InfoBoxFieldProps object containing titleText and optional contentText
+ * @returns JSX.Element - The rendered InfoBoxField component
+ */
+export const InfoBoxField = (props: InfoBoxFieldProps) => {
+  return (
+    <div className="justify-left flex flex-row items-center space-x-8">
+      <InfoBoxFieldTitle titleText={props.titleText} />
+      <p className="text-[16px] font-[BC_Sans]">{props.contentText}</p>
+    </div>
+  );
+};
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,23 +1,42 @@
 import { Heading, Text } from '@bcgov/design-system-react-components';
+import { GoldBar } from '../components/element/GoldBar';
 import { BaseLayout } from '../components/layout/BaseLayout';
+import {
+  InfoBox,
+  InfoBoxField,
+} from '../components/element/InfoBox';
 
 export const Home = () => {
   return (
     <BaseLayout>
-      <Heading level={1}>Home Page</Heading>
-      <Text>
-        <p>
-          This page uses the `Base Layout` wrapper allowing content to appear
-          between the header and footer.
-        </p>
-        <br />
-        <Heading level={3}>More Content</Heading>
-        <p>
-          This page is the landing page. Currently it isnt very exciting. But we
-          can use this to test building out components and will build onto it as
-          we go.
-        </p>
-      </Text>
+      <div className="col-start-2 col-end-8 grid">
+        <GoldBar />
+        <Heading level={1}>Manage your BCGW Oracle account</Heading>
+        <Text>
+          <p>
+            The Database User Management Tool (DBUMT) is managed by Data
+            Publication Services team in BC Data Service. This tool is designed
+            to help users to securely update their BC Geographic Warehouse
+            (BCGW) Oracle account database(s) password(s) and ask for account
+            details. To use this tool please enter your Oracle account username.
+            In most cases, the username for your BCGW account matches your IDIR.
+          </p>
+          <br />
+        </Text>
+      </div>
+      <br />
+      <div className="col-start-2 sm:col-end-8 md:col-end-6">
+        <InfoBox header="BC Geographic Warehouse Oracle Account Information">
+          <InfoBoxField
+            titleText="BCGW Account/Username:"
+            contentText="TEST HOLDER"
+          />
+          <br />
+        </InfoBox>
+
+        <GoldBar />
+        <p>NAV BUTTONS WILL GO HERE</p>
+      </div>
     </BaseLayout>
   );
 };
