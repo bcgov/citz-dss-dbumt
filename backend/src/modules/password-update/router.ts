0de5b3844bba07d22aa85a1e1b918e8eef53bc6d
Molnar <Stuart.Molnar@gov.bc.ca>
2025-04-25 08:01:25 -0700

    Add passwordUpdate routing

--- backend/src/modules/password-update/router.ts
+++ backend/src/modules/password-update/router.ts
@@ -4,10 +4,17 @@ import {
   findReqByName,
   getAllReq,
 } from "./controllers/requirements";
+import {
+  checkPassword,
+  updateUserPassword,
+} from "./controllers/passwordUpdate";
 
 const router = Router();
 
 router.route(`/requirement`).get(getAllReq).post(addRequirement);
 router.route(`/requirement/:name`).get(findReqByName);
+
+router.post("/check", checkPassword);
+router.post("/update", updateUserPassword);
 
 export default router;
