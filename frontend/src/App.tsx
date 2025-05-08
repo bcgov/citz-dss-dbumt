1719fd5e4739810ad0d888da9625b15eb62fa676
Molnar <Stuart.Molnar@gov.bc.ca>
2025-05-08 16:26:08 -0700

    Add ChangePassword and AccountQuery Routing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,8 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Home } from './pages/Home';
 import { Login } from './pages/Login';
+import { AccountQuery } from './pages/AccountQuery';
+import { ChangePassword } from './pages/ChangePassword';
 
 function App() {
   return (
@@ -8,6 +10,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/accountquery" element={<AccountQuery />} />
+        <Route path="/changepassword" element={<ChangePassword />} />
       </Routes>
     </Router>
   );
