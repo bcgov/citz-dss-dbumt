83ab2612832403c35f853f8966cd9e6e6bf48734
Taylor Friesen <taylor.friesen@gov.bc.ca>
2025-05-08 08:45:43 -0700

    update to health endpoints

--- backend/src/modules/health/router.ts
+++ backend/src/modules/health/router.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
-import { serverHealth } from "./controller.ts";
+import { HTTP_STATUS_CODES } from "@/constants";
 
 const router = Router();
 
-router.get("/server", serverHealth);
+router.route("/server").get((req, res) => {
+  return res.status(HTTP_STATUS_CODES.OK).send("Server Healthy");
+});
 
 export default router;
--- backend/src/modules/password-update/router.ts
+++ backend/src/modules/password-update/router.ts
@@ -4,10 +4,16 @@ import {
   findReqByName,
   getAllReq,
 } from "./controllers/requirements";
+import { HTTP_STATUS_CODES } from "@/constants";
 
 const router = Router();
 
 router.route(`/requirement`).get(getAllReq).post(addRequirement);
 router.route(`/requirement/:name`).get(findReqByName);
+
+// Health check route
+router.route('/health').get((req, res) => {
+  return res.status(HTTP_STATUS_CODES.OK).send("PAssword Update Healthy");
+});
 
 export default router;
