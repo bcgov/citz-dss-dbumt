cd5b78363da2b355e494b9a4132a208d926ec575
Taylor Friesen <taylor.friesen@gov.bc.ca>
2025-05-14 11:35:33 -0700

    testing frontend for sure this time

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -3,7 +3,11 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 
 // testing frontend build and push action
-
+// unsure why this is not triggering the action
+function test() {
+  console.log("test");
+}
+test();
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
