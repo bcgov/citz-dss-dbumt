b45e95d83d9caa314a7a2066ad66d113f6652773
Molnar <Stuart.Molnar@gov.bc.ca>
2025-07-14 16:45:03 -0700

    Add change password form to page

--- frontend/src/pages/ChangePassword.tsx
+++ frontend/src/pages/ChangePassword.tsx
@@ -2,8 +2,26 @@ import { BaseLayout } from '../components/layout/BaseLayout';
 import { PageTitleInfo } from '../components/layout/PageTitleInfo';
 import { BreadcrumbItem } from '../components/element/Breadcrumb';
 import { Text as BCGovText } from '@bcgov/design-system-react-components';
+import { ChangePasswordForm } from '../components/element/ChangePasswordForm';
+import { useEffect } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
 
 export const ChangePassword = () => {
+  // Pass oracleid to next page
+  const location = useLocation();
+  const oracleId = location.state?.oracleId;
+  // Project navigation
+  const navigate = useNavigate();
+
+  // If no oracleid, go to login page
+  useEffect(() => {
+    if (!oracleId) {
+      navigate('/login', { replace: true });
+    }
+  }, [oracleId, navigate]);
+
+  if (!oracleId) return null;
+
   const title = 'Change your BCGW Oracle password';
   const collapseText = ``;
   const text = (
@@ -32,6 +50,7 @@ export const ChangePassword = () => {
     <BaseLayout breadcrumbItems={breadcrumbItems}>
       <PageTitleInfo title={title} collapseText={collapseText} text={text} />
       <br />
+      <ChangePasswordForm oracleId={oracleId} />
     </BaseLayout>
   );
 };
